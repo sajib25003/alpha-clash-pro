d73c15f1e16c368de23ae77e5e13a502decabf57
Md. Ashik Hassan Bhuiyan <sjb.25003@gmail.com>
2024-02-17 01:13:53 +0600

    final update

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -7,6 +7,63 @@
 //     playgroundScreen.classList.remove('hidden')
 // }
 
+function handleKeyboardButtonPress(event) {
+  const playerPressed = event.key;
+  // console.log("player pressed ", playerPressed);
+  if (playerPressed === 'Escape'){
+    gameOver();
+  }
+
+  // get the expected pressed
+  const currentAlphabetElement = document.getElementById("current-alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLowerCase();
+  console.log(playerPressed, expectedAlphabet);
+
+  // game logic and score update and continue the game
+  if (playerPressed === expectedAlphabet) {
+    const currentScore = getTextElementById("current-score");
+    const newScore = currentScore + 1;
+    setTextElementById("current-score", newScore);
+ 
+
+    // --------ALTERNATIVE------------
+    // //update score
+    // // 1. get the current score
+    // const currentScoreElement = document.getElementById("current-score");
+    // const currentScoreText = currentScoreElement.innerText;
+    // const currentScore = parseInt(currentScoreText);
+
+    // // 2. update the score
+    // const newScore = currentScore + 1;
+
+    // // // 3. show the updated score
+    // currentScoreElement.innerText = newScore;
+    // start new round
+    removeBackgroundById(expectedAlphabet);
+    continueGame();
+  } else {
+    // update life count
+    const currentLife = getTextElementById("current-life");
+    const newLife = currentLife - 1;
+    setTextElementById("current-life", newLife);
+    if (newLife === 0) {
+      gameOver();
+    }
+
+    // // 1. get the life-count element
+    // const currentLifeElement = document.getElementById('current-life');
+    // const currentLifetext = currentLifeElement.innerText;
+    // const currentLife = parseInt(currentLifetext);
+    // // 2. update the life count
+    // const newLife = currentLife -1;
+    // // 3. update the current life element
+    // currentLifeElement.innerText = newLife;
+  }
+}
+// capture keyboard key press
+document.addEventListener("keyup", handleKeyboardButtonPress);
+
 function continueGame() {
   // generate a random alphabet
   const alphabet = getARandomAlphabet();
@@ -21,7 +78,26 @@ function continueGame() {
 }
 
 function play(elementId) {
+  // hide all screen other than playground
   addClassById("home-screen");
+  addClassById('game-over')
   removeClassById("playground-screen");
+
+  // reset score and life
+  setTextElementById('current-score', 0);
+  setTextElementById('current-life', 5);
+
+
   continueGame();
+}
+
+function gameOver() {
+  addClassById("playground-screen");
+  removeClassById("game-over");
+  // final score update
+  const finalScore = getTextElementById('current-score');
+  setTextElementById('final-score', finalScore);
+  // remove hightlight of last alphabet
+  const currentAlphabet = getElementById('current-alphabet');
+  removeBackgroundById(currentAlphabet);
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,3 +1,17 @@
+// get text element by id
+function getTextElementById(elementId){
+  const element = document.getElementById(elementId);
+  const elementtext = element.innerText;
+  const elementValue = parseInt(elementtext);
+  return elementValue;
+}
+
+// set text element by id
+function setTextElementById(elementId,value){
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+}
+
 // add a class from an element
 function addClassById(elementId) {
   const element = document.getElementById(elementId);
@@ -14,6 +28,18 @@ function removeClassById(elementId) {
 function setBackgroundById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-300') 
+}
+// remove background by Id
+function removeBackgroundById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-300') 
+}
+
+// get element by id
+function getElementById(elementId){
+  const element = document.getElementById(elementId);
+  const text = element.innerText;
+  return text;
 }
 
 // get a random alphabet
